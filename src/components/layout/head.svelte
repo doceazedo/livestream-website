8c7cf4977546d4dfc8a3a1e68bc37cbf6b37229e
DoceAzedo <oi@lucasfernandes.com.br>
2021-10-08 16:01:32 -0300

    🚧 Criar página /carreira (WIP)

--- src/components/layout/head.svelte
+++ src/components/layout/head.svelte
@@ -7,4 +7,4 @@
 <link rel="stylesheet" href="/assets/css/global.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,700;1,400&display=swap">
